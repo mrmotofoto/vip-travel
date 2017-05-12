746b29851422bb315b81c95115b5b3ea3b21a855
Rich <richardstiehm@gmail.com>
2017-05-12 18:09:07 -0400

    Responsive Images Working For Hero Section

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+// const gulp = require('gulp');
+// const watch = require('gulp-watch');
+//const postcss = require('gulp-postcss');
+// const autoprefixer = require('autoprefixer');
+// const cssvars = require('postcss-simple-vars');
+// const nested = require('postcss-nested');
+// const cssImport = require('postcss-import');
+// const browserSync = require('browser-sync').create();
